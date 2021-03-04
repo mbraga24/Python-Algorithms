c52bfd5a8feee2e35d74cb3bb8a2de18142b3662
github username <rdg.brg@gmail.com>
2021-03-04 14:25:28 -0500

    SortedSquaredArray solution 1

--- sortedSquaredArray.py
+++ sortedSquaredArray.py
@@ -1,3 +1,12 @@
+# Solution 1:
+
+# Time Complexity: O(nlogn)
+  # Event though we loop over the given array in O(n) when we handle negative numbers the array will not
+  # be in ascending order so we will need sort() the array before returning it.
+  # This brute force approach in the end is: O(nlogn)
+# Space Complexity: O(n)
+  # The space complexity is O(n) because we are creating a new array to store the sorted squared values.
+
 def sortedSquaredArray(array): 
   sortedSquared = [0 for _ in array]
 
