699a0f512116225a76186508dada09ba2498a92c
github username <rdg.brg@gmail.com>
2021-03-05 19:11:40 -0500

    nonConstructibleChange solution

--- nonConstructibleChange.py
+++ nonConstructibleChange.py
@@ -1,3 +1,12 @@
+# Time Complexity: O(nlogn)
+  # Because we need to sort the coins array we will have O(nlogn) time. We will still iterate once 
+  # over the coins array at O(n) time. The equation would be: O(n log n) + n
+  # But since 'n' is less than 'n log n' time we remove 'n' and just describe the time complexity as
+  # O(nlogn) time
+
+# Space Complexity: O(1)
+  # We are not using any extra space in memory to sort the coins array. We are sorting it in place.
+
 def nonConstructibleChange(coins):
   coins.sort()
 
